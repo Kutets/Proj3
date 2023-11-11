1da2545f3e178974a4754a41a489aba629dc67d6
Luiz Fernando <145866661+Kutets@users.noreply.github.com>
2023-11-11 16:05:22 -0300

    Atualização da main.c

--- main.c
+++ main.c
@@ -45,8 +45,12 @@ int main() {
       case 3:
         listarTarefas(lista);
         break;
-      case 4;
-        
+      case 4:
+        printf("Detalhes da tarefa antes da edição:\n");
+        printTarefa(&(lista.tarefas[0]));
+        editarTarefa(&lista, 0);
+        salvarLista(lista, "tarefas.bin");
+        break;
       case 5:
         sair = 1;
         break;
